ff83865d93aca5803624d4fce42ae4dd6ab84514
nabinhamal <nabinhamal2@gmail.com>
2023-10-05 15:02:57 +0545

    updated

---
+++ server/models/User.js
@@ -0,0 +1,23 @@
+// user.js
+const Mongoose = require("mongoose")
+const UserSchema = new Mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    minlength: 6,
+    required: true,
+  },
+  role: {
+    type: String,
+    default: "Admin",
+    required: true,
+  },
+})
+
+
+const User = Mongoose.model("user", UserSchema)
+module.exports = User
